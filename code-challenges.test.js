427ae9563d34cd11864687285ff7a0cc2aaca3c7
CSapien08 <sapiano08@gmail.com>
2021-07-26 02:27:49 -0700

    Week 3 HW

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 3: Coding practical questions with Jest
 
+//const { iteratee } = require("lodash")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -24,15 +26,67 @@
 // Example input: 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
+//Create a test function.
+//  use describe() to pass in the name of the function, fibonacciFunction
+//  use it() to describe what fibonacciFunction is doing
+//      within it() write the test using expect and toEquals
+//      test to make sure it returns the expected output
+//END
 
+describe("fibonacciFunction", () => {
+    it("Takes in a number that is greater than 2 and return an array of that length containing the Fibonacci sequence", () => {
+        expect(fibonacciFunction(6)).toEqual([1, 1, 2, 3, 5, 8])
+        expect(fibonacciFunction(10)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+    })
+
+})
 
 // b) Create the function that makes the test pass.
 
+//Create a function called fibonaccitFunction
+//  parameters - a integer
+//  check to make sure the arguement is greater than two
+//      if true then return an array containing the Fibonacci sequence for that number
+//      else return 'invaild arguement'
+//END
 
+const fibonacciFunction = (int) => {
+    var array = new Array(int)
+    if(int < 2){
+        return 'invaild arguement'
+    }
+    else{
+        array[0] = 1;
+        array[1] = 1;
+        for(let i = 2; i < array.length; i++){
+            array[i] = array[i - 1] + array[i - 2];
+        }
+    }
+    return array;
+}
+
+console.log(fibonacciFunction(6))
+console.log(fibonacciFunction(10))
+console.log(fibonacciFunction(1))
 
 // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+//Create a test function.
+//  use describe() to pass in the name of the function, oddNumbers
+//  use it() to describe what oddNumbers is doing
+//      within it() write the test using expect and toEquals
+//      test to make sure it returns the expected output
+//END
+
+describe("oddNumbers", () => {
+    it("Takes in an array and returns a new array of only odd numbers sorted from least to greatest.", () => {
+        expect(oddNumbers(fullArr1)).toEqual([-9, 7, 9, 199])
+        expect(oddNumbers(fullArr2)).toEqual([-823, 7, 23])
+    })
+
+})
 
 var fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
 // Expected output: [-9, 7, 9, 199]
@@ -43,11 +97,57 @@ var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
 
 // b) Create the function that makes the test pass.
 
+//Create a function called oddNumbers
+//  parameters - an array
+//  using filter, iterate through the array
+//      check the typeof the index
+//          if the typeof is a number, use the modulo to test for odd numbers and return the odd number
+//          else skip the value
+//  return the values sorted using sort().
+//END
 
+const oddNumbers = (array) => {
+    return array.filter(value => {
+        if(typeof value === 'number' && value %2 !== 0){
+            return value
+        }
+    }).sort((a, b) => a - b);
+}
+
+console.log(oddNumbers(fullArr1))
+console.log(oddNumbers(fullArr2))
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
+
+/*
+
+
+Austin, i am consoling the right answer but whenever I run my test it says it fails. It was past midnight when I looked at this again so I figured it was too late
+so I will ask you in the moring.
+
+
+*/
+
+
+
 // a) Create a test with expect statements for each of the variables provided.
+
+//Create a test function.
+//  use describe() to pass in the name of the function, accumalte
+//  use it() to describe what accumalte is doing
+//      within it() write the test using expect and toEquals
+//      test to make sure it returns the expected output
+//END
+
+describe("accumalte", () => {
+    it("Takes in an array and returns an array of the accumulating sum.", () => {
+        //expect(accumalte(numbersToAdd1)).toEqual([2, 6, 51, 60])
+        //expect(accumalte(numbersToAdd2)).toEqual([0, 7, -1, 11])
+        //expect(accumalte(numbersToAdd3)).toEqual([])
+        expect(accumalte(numbersToAdd1)).toEqual([2, 6, 51, 60])
+    })
+})
 
 var numbersToAdd1 = [2, 4, 45, 9]
 // Excpected output: [2, 6, 51, 60]
@@ -58,6 +158,41 @@ var numbersToAdd2 = [0, 7, -8, 12]
 var numbersToAdd3 = []
 // Expected output: []
 
-
-
 // b) Create the function that makes the test pass.
+
+//Create a function called accumlate
+//  parameters - an array of numbers
+//  use map to iterate over the array
+//      take the current value and add it to the previous
+//      if there isn't a current value then add 0
+//  return the array
+
+// const accumalte = (array) => {
+//     return array.map((value, index, arr) => {
+//         if(index === 0){
+//             return value;
+//         }
+//         else{
+//             arr[index] = arr[index] + arr[index - 1]
+//             value = arr[index]
+//             return value
+//         }
+//     })
+// }
+const accumalte = (array) => {
+    var temp = []
+    for(let i = 0; i < array.length; i++){
+        if(i == 0){
+            temp[i] = array[i];
+        }
+        else{
+            array[i] = array[i] + array[i - 1]
+            temp[i] = array[i]
+              
+        }
+    }
+    return temp
+}
+console.log(accumalte(numbersToAdd1))
+console.log(accumalte(numbersToAdd2))
+console.log(accumalte(numbersToAdd3))
--- interview-questions.md
+++ interview-questions.md
@@ -9,59 +9,59 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is `this` in JavaScript?
 
-  Your answer:
+  Your answer: The keyword 'this' refers to the object that its within.
 
-  Researched answer:
+  Researched answer: It is a property of execution, meaning that at runtime it is set to the value of the funtions's this, or essientially just refers to the object.
 
 
 
 2. What is yarn? What file(s) are modified in a React application when you run yarn?
 
-  Your answer:
+  Your answer: Yarn is a package manager like npm but better.
 
-  Researched answer:
+  Researched answer: Yarn or 'Yet Another Resource Negotiator' helps manage project dependencies like Jest for example. I belive they modify the yarn.lock file.
 
 
 
 3. What are props in React?
 
-  Your answer:
+  Your answer: Props or properties allow users to pass information to a Components.
 
-  Researched answer:
+  Researched answer: React Props are function arguements and then attributes in HTML via attributes
 
 
 
 4. What is JSX?
 
-  Your answer:
+  Your answer: JavaScript XML, it allows users to write and use JavaScript in React
 
-  Researched answer:
+  Researched answer: It is a syntax extenstion to JavaScript and it used to describe what the UI should look like, it has the capability of full JS. It is faster than normal JS and it allows users to directly write HTML in react.
 
 
 
 5. What is a DOM event?
 
-  Your answer:
+  Your answer: Not sure, maybe the DOM changes whenever there is an event fired off, like a mouse click.
 
-  Researched answer:
+  Researched answer: DOM events is a way to handle events like a mouse click or form being submitted, it is a signal to let the DOM know something is occuring.
 
 
 
 6. STRETCH: What is an anonymous function in JavaScript?
 
-  Your answer:
+  Your answer: An anonymous function is a function that does not have a name or it is declared without a name. I see it mainly being used as a shortcut inside of already defined functions.
 
-  Researched answer:
+  Researched answer: It is in face a function without a name. They are not accessible after it is declared and strange behavior happens when its used by itself and not inside a defined funtion.
 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Conditional rendering:
+1. Conditional rendering: Allows users to use JavaScript operators like if or the conditional operator in React
 
-2. Ruby:
+2. Ruby: Ruby is an interpreted, high-level, general-purpose programming language desined to help programmers be more productive, it is mainly used to build web applications
 
-3. Object oriented programming:
+3. Object oriented programming: OOP is a programming concept that organizes its data around obects. 
 
-4. Ruby hash:
+4. Ruby hash: A hash map but in Ruby
 
-5. Ruby blocks:
+5. Ruby blocks: They are anonymous functions that can be passsed into methods.
